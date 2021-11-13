76dd15d4919aead5c2f208b414d11e552cf78896
Artem <gaidash2303@gmail.com>
2021-11-13 19:51:01 +0300

    devArtem: v0.0 ( initial app )

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,24 @@
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        <p>Mean</p>
+        <p>0</p>
+      </div>
+      <div>
+        <p>Median</p>
+        <p>0</p>
+      </div>
+      <div>
+        <p>StdDev</p>
+        <p>0</p>
+      </div>
+      <div>
+        <p>Mode</p>
+        <p>0</p>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/data-control/control.js
@@ -0,0 +1,13 @@
+const DataControl = () => {
+  return (
+    <div>
+      <button>Update</button>
+      <form>
+        <input placeholder="Enter the number"></input>
+        <button>Enter</button>
+      </form>
+    </div>
+  );
+};
+
+export default DataControl;
